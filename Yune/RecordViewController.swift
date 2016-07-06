f5a41a2fe272b6d412c29b597b53bf0e47dd7832
bryanye97 <bryanye97@gmail.com>
2016-07-06 13:52:58 -0700

    Added video functionality

--- Yune/RecordViewController.swift
+++ Yune/RecordViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class RecordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       startCameraFromViewController(self, withDelegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +22,21 @@ class RecordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func video(videoPath: NSString, didFinishSavingWithError error: NSError?, contextInfo info: AnyObject) {
+        var title = "Success"
+        var message = "Video was saved"
+        if let _ = error {
+            title = "Error"
+            message = "Video failed to save"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
+    @IBAction func record(sender: AnyObject) {
+        startCameraFromViewController(self, withDelegate: self)
+    }
     /*
     // MARK: - Navigation
 
@@ -32,4 +47,36 @@ class RecordViewController: UIViewController {
     }
     */
 
+}
+
+extension RecordViewController: UIImagePickerControllerDelegate {
+    
+    func startCameraFromViewController(viewController: UIViewController, withDelegate delegate: protocol<UIImagePickerControllerDelegate, UINavigationControllerDelegate>) -> Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) == false {
+            return false
+        }
+        
+        let cameraController = UIImagePickerController()
+        cameraController.sourceType = .Camera
+        cameraController.mediaTypes = [kUTTypeMovie as NSString as String]
+        cameraController.allowsEditing = false
+        cameraController.delegate = delegate
+        
+        presentViewController(cameraController, animated: true, completion: nil)
+        return true
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        dismissViewControllerAnimated(true, completion: nil)
+        if mediaType == kUTTypeMovie {
+            guard let path = (info[UIImagePickerControllerMediaURL] as! NSURL).path else { return }
+            if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path) {
+                UISaveVideoAtPathToSavedPhotosAlbum(path, self, #selector(RecordViewController.video(_:didFinishSavingWithError:contextInfo:)), nil)
+            }
+        }
+    }
+}
+
+extension RecordViewController: UINavigationControllerDelegate {
 }
